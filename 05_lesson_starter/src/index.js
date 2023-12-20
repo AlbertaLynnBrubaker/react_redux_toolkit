8968b6910d0addbfd809bb602a0d294ce5d7b560
Alie Brubaker <findingalberta@gmail.com>
2023-12-19 17:49:39 -0800

    Finished lesson 5

--- 05_lesson_starter/src/index.js
+++ 05_lesson_starter/src/index.js
@@ -12,14 +12,14 @@ store.dispatch(fetchPosts());
 store.dispatch(fetchUsers());
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <Router>
-        <Routes>
-          <Route path="/*" element={<App />} />
-        </Routes>
-      </Router>
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Provider store={store}>
+            <Router>
+                <Routes>
+                    <Route path="/*" element={<App />} />
+                </Routes>
+            </Router>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
